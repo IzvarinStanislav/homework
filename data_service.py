7183d6de7b2198a70d5d8c1598b43a56f9f1b265
IzvarinStanislav <S.Izvarin_FIT_14_21_B_d@knute.edu.ua>
2021-12-03 11:21:36 +0200

    changes in data_service

--- data_service.py
+++ data_service.py
@@ -7,6 +7,7 @@ def get_rates():
     повертає список курсів валют
     """
 
+
     # накопичення даних файлу у списку
     with open("./data/rate.csv", 'r') as f:
         rates = f.readlines()
@@ -16,6 +17,7 @@ def get_rates():
     # порізати в циклі строки на окремі елементи
     for rate in rates:
         obj = split_line(rate)
+        obj[0] = int(obj[0])
         rates_splitted.append(obj)
 
     return rates_splitted
@@ -25,10 +27,7 @@ def split_line(line):
     object = line.split(',')
     return object
 
-
-# модуль призначено для роботи з зовнішнім файлами
-# читання та виведення візуального контролю
-
+# читання файлу найменувань банків 'directories'
 def get_directories ():
     """читання файла довідника ринків 'directories'
     та формування списку ринків
@@ -40,16 +39,47 @@ def get_directories ():
         directories = f.readlines()
 
     # підготовка даних для подальшої обробки
-    directories_splitted =[]
-    # порізати в циклі строки на окремі елементи
-    for directory in directories:
-        object = split_line(directory)
-        directories_splitted.append(object)
+        directories_splitted =[]
+        # порізати в циклі строки на окремі елементи
+        for directory in directories:
+            obj = split_line(directory)
+            directories_splitted.append(obj)
 
     return directories_splitted
+
+# вивід списку найменувань банків
+def show_directories(directories):
+
+    # задати інтервал виводу
+    directory_code_from = input("З якого кода клієнта?")
+    directory_code_to = input("По який кода клієнта?")
+
+    lines_found=0
+
+    for directory in directories:
+        if directory_code_from <= directory[0] <= directory_code_to:
+            print ("Код банку: {:5} Назва: {:25}".format(*directory))
+            lines_found += 1
+
+        if lines_found == 0:
+            print ("Банків по Вашому запиту не знайдено")
+
+def show_rates(rates):
+
+    for rate in rates:
+        print("Код банку:{:3} Доллари США {:4} Марки ФРН {:4} Рік{:4}".format(rate[1], rate[2], rate[3], rate[4]))
+
+
+def split_line(line):
+    """повертає список об'єктів з строки"""
+    object = line.split(',')
+    return object
 
 if __name__ == '__main__':
     rates = get_rates
     directories = get_directories
-    pass
     
+    show_directories(directories)
+    
+    pass
+   
